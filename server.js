a5f9238b1c756e1d594c4f4dc47372f346af64d0
guswnsakvk <guswnsakvk1@naver.com>
2022-11-03 17:58:19 +0900

    mongodb 연결 시도 중

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended : true}))
 
-app.listen(8080, function() {
+let db
+const MongoClient = require('mongodb').MongoClient
+MongoClient.connect("mongodb+srv://admin:qwer1234@cluster0.k5lltli.mongodb.net/?retryWrites=true&w=majority", function(error, client){
+  db = client.db('security_login')
+
+  db.collection('user').insertOne({이름 : "현준", 나이 : 23}, function(error, result){
+    console.log('저장완료')
+  })
+  
+  app.listen(8080, function() {
     console.log('listening on 8080')
+  })
 })
 
 app.get('/pet', function(요청, 응답) { 
